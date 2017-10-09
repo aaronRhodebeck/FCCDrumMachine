bbbb8ce0c239a13da4a4783db5e8de88c642d96d
aaronRhodebeck <arhodebeck@gmail.com>
2017-10-09 14:42:27 -0500

    Add color object with shared app colors

---
+++ app/PresentationComponents.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import React from "react";
+
+const appColors = {
+  lightPurple: "rgb(111, 124, 178)",
+  gray: "rgb(216, 225, 225)",
+  lightBlueGreen: "rgb(91, 159, 159)",
+  blue: "rgb(64, 81, 151)",
+  darkBlue: "rgb(29, 49, 132)",
+  lightGreen: "rgb(143, 217, 124)"
+};
